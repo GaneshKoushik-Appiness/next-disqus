b6036b0b26d8bd4f8ce7ad3286750bfafbf47a8f
Appy <Appy@apples-MacBook-Air.local>
2022-06-23 17:42:26 +0530

    build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { DiscussionEmbed } from 'disqus-react';
 import { CommentCount } from 'disqus-react';
-import Script from 'next/script'
+import Link from 'next/script'
+import Image from 'next/image'
 import Head from 'next/head'
 import styles from "../styles/Home.module.css"
 
@@ -11,12 +12,12 @@ export default function Home() {
                 <title>Disqus Comment</title>
                 <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" />
                 <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
             </Head>
             <div className='d-flex justify-content-center'>
-                <img src='https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Comment-edit-icon.png' className={styles.comment_icon}></img>
+                <Image src='https://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/512/Comment-edit-icon.png' className={styles.comment_icon}></Image>
             </div>
             <div className='text-center mb-4 font-weight-bold fs-30'>
                 <CommentCount 
